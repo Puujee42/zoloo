5566609ee4fdb172693c083e451a76a7492b5e73
Puujee42 <your-github-email@example.com>
2025-08-09 20:22:06 +0800

    fix: REMOVE incorrect next/utils import from User model

--- models/User.js
+++ models/User.js
@@ -1,16 +1,19 @@
+// models/User.js
+
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
-const userSchema =  new mongoose.Schema(
-    {
-        _id:{type: String, required:true},
-        name:{type: String, required:true},
-        email:{type:String, required:true, unique:true},
-        imageUrl: {type:String, required:true},
-        cartItems: {type:Object, default:{}}
 
+const userSchema = new mongoose.Schema(
+    {
+        _id: { type: String, required: true },
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        imageUrl: { type: String, required: true },
+        cartItems: { type: Object, default: {} }
     },
-    {minimize:false}
-)
-const User = mongoose.models.user || mongoose.model('user',userSchema)
-export default User
+    { minimize: false }
+);
+
+const User = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default User;
